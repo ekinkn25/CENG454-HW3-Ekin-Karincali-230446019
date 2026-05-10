a355c090ddb1ceaca094e7c76195bbf542ca154f
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-05-10 22:47:53 +0300

    feat:implement-GameEvents-observer-system-with-core-health-enemy-death,-and-wave-events

---
+++ Assets/Scripts/Patterns/Observer/GameEvents.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+namespace CoreBreach.Patterns.Observer
+{
+    //observer is like event handler for game
+    //no system dont have to know directly other systems
+    //when something happens, it will reported here and listeners reacts
+    public static class GameEvents
+    {
+        //core events:
+
+        //it will get triggered when core get take damage or recover 
+        //float currentHealth -> healt at the moment, float maxHealth -> max healt is used for HUD percentage calculations
+        //who fires: CoreHealth.TakeDamage()
+        //who listens: HUDController, AudioManager
+        public static Action<float, float> OnCoreHealthChanged;
+
+        //it will get triggered when core's health is zero
+        //who fires: CoreHealth.TakeDamage()
+        //who listens: GameManager (used for "you losed" screen)
+        public static Action OnCoreDead;
+
+
+        //enemy events: 
+
+        //triggered when an enemy dies
+        //Vector3 position -> posiition for efect or loot spawn
+        //who fires: EnemyHealth.TakeDamage()
+        //who listens: ScoreManager, WaveManager(remainin number)
+        //TODO: you can add score number for parameter -> Action<Vector3, int>
+        public static Action<Vector3> OnEnemyDied;
+
+
+        //wawe GameEvents
+        //it triggers when a wawe is completed
+        //int waweNumber ->  which wawe is completed
+        //who fires: WaveManager
+        //who listens: GameManager, HUDController
+        //TODO: if it is last wave it may be trigger the OnGameWon
+        public static Action<int> OnWaveCompleted;
+
+        //it triggers when all waves are finished and game is woned
+        //who fires: WaveManager
+        //who listens: GameManager (wining screen)
+        //TODO when WaveManager added, invoke that event from there
+        public static Action OnGameWon;
+
+
+        //security: cleaning in screen changes
+        //when screen loads again old subscribers are cleaned.
+        //it is called in GameManager
+        //TODO: when we write GameManager, OnEnable can be called inside
+        public static void ResetAllEvents()
+        {
+            OnCoreHealthChanged = null;
+            OnCoreDead          = null;
+            OnEnemyDied         = null;
+            OnWaveCompleted     = null;
+            OnGameWon           = null;
+
+            Debug.Log("[GameEvents] All events cleaned.");
+        }
+    
+    }
+}
