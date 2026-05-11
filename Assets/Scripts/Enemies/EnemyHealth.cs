ef2002fe4e7dc5d2b70ed4c83f2782e7f0872a4d
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-05-11 09:54:15 +0300

    feat:EnemyHealth-impelments-IDamagable-and-fires-OnEnemyDied

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using CoreBreach.Interfaces;
+using CoreBreach.Patterns.Observer;
+using System.Xml.Serialization;
+
+namespace CoreBreach.Enemies
+{
+    public class EnemyHealth : MonoBehaviour, IDamageable, IPoolable
+    {
+        [SerializeField] private float maxHealth=30f;
+        [SerializeField] private float currentHealth;
+        private bool _isDead = false; // to prevent 2nd die event
+
+        
+        //IPoolable -it will call when you pull on pool- life and flag become zero
+        //TODO: ObjectPool system will be implemented later on this project
+        //pool manager will call this method automatically
+        public void OnSpawn()
+        {
+            currentHealth = maxHealth;
+            _isDead = false;
+            gameObject.SetActive(true);
+            Debug.Log($"[Enemy Health] did spawn. Life: {currentHealth}");
+        }
+        
+        //called when it turneds back to pool 
+        //TODO: when ObejctPool system implemented "Destroy(gameObject)" will be "EnemyPool.Instance.ReturnToPool(this);"
+        public void OnDespawn()
+        {
+            gameObject.SetActive(false);
+            Debug.Log($"[EnemyHealth] returned to ppol : {gameObject.name}");
+        }
+
+
+        //IDamagable
+        public void TakeDamage(float amount)
+        {
+            if (_isDead) return;
+            currentHealth -= amount;
+            currentHealth = Mathf.Max(currentHealth, 0f);
+            Debug.Log($"[EnemyHealth] taken damage: {amount} | Remaining life: {currentHealth} /{maxHealth}");
+
+            if (IsDead())
+            {
+                _isDead = true;
+                HandleDeath();
+            }
+        }
+
+        public bool IsDead()
+        {
+            return currentHealth <= 0f;
+        }
+
+        private void HandleDeath()
+        {
+            Debug.Log($"[EnemyHealth] Enemy died: {gameObject.name}");
+
+            //OBSERVER: WaveManager kalan düşman sayısını azaltır Score MAnager puan ekler HUDController Score günceller ama bu sınıf onların hiç birini tanımıypr sadece duyuruyor
+            GameEvents.OnEnemyDied?.Invoke(transform.position);
+
+            //TODO: Object Pool eklenince aşağıdaki destor değişecek yerine OnDespawn(); ve Wave maanagerdan pool referansı gelecek
+            Destroy(gameObject);
+        }
+
+        private void Start()
+        {
+            //Pool system geldipğinde Start() kaldır OnSpawn her spawnda zaten çağrılacak
+            currentHealth = maxHealth;
+            _isDead = false;
+        }
+
+        [ContextMenu("Test: 10 hasar ver")]
+        private void TestTakeDamage()
+        {
+            TakeDamage(10f);
+        }
+    }
+}
