d872f24b1237f49f8463e0bc4a1a05ebf2185d9c
Ekin Karıncalı <s230446019@stu.thk.edu.tr>
2026-05-13 16:57:34 +0300

    feat:implement-decorator-for-weapon

--- Assets/Scripts/Patterns/Decorator/WeaponDecorator.cs
+++ Assets/Scripts/Patterns/Decorator/WeaponDecorator.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using CoreBreach.Interfaces;
 
-public class WeaponDecorator : MonoBehaviour
+namespace CoreBreach.Patterns.Decorator
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Base class of all decorators
+    // IWeaponBehavior implements this and PlayerShooter knows this as an interface
 
-    // Update is called once per frame
-    void Update()
+    // _wrapped: wrapped weapon reference — it can be BasicWeapon or another decorators
+    // default behavior: delegate everything to the wrapped weapon
+    // sub classes overrides the method that they want to change
+    public abstract class WeaponDecorator : IWeaponBehavior
     {
-        
+        // Protected: accessible for sub classes
+        protected readonly IWeaponBehavior _wrapped;
+
+        protected WeaponDecorator(IWeaponBehavior wrapped)
+        {
+            _wrapped = wrapped;
+        }
+
+        // default: delegate to wrapped weapon
+        // if you dont override this will work
+        public virtual void Fire(Vector3 origin, Vector3 direction)
+        {
+            _wrapped.Fire(origin, direction);
+        }
+
+        // default: return the value of wrapped weapon's damage
+        // DoubleDamageDecorator will override this
+        public virtual float GetDamage()
+        {
+            return _wrapped.GetDamage();
+        }
     }
 }
